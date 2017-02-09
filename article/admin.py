9acd828026798587e919c5c3453ba39678c5fe0e
Yolshin195 <elshin195@gmail.com>
2017-02-09 17:33:08 +0500

    Полностью настроен Timymce

--- article/admin.py
+++ article/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from article.models import Article, TableOfContents
 
+class CollectionAdmin(admin.ModelAdmin):
+ 
+    # .. какие-то настройки отображения
+ 
+    class Media:
+        js = (
+            '/static/js/tinymce/tinymce.min.js',
+            '/static/js/tinymce/tinymce.init.js',
+        )
 # Register your models here.
 admin.site.register(TableOfContents)
-admin.site.register(Article)
+admin.site.register(Article, CollectionAdmin)
