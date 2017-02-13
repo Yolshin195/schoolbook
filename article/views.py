29712fcf0b4033955076be8281f356112ed38dde
Yolshin195 <elshin195@gmail.com>
2017-02-13 21:59:51 +0500

    Исправил ошибку отображения названия предмета в списке тем этого придмета

--- article/views.py
+++ article/views.py
@@ -14,6 +14,13 @@ class ArticleListView(ListView): # представление в виде спи
     def get_queryset(self):
         return Article.objects.filter(article_tableofcontent_id=self.kwargs['pk'])                   # модель для представления 
 
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(ArticleListView, self).get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['TableOfContents'] = TableOfContents.objects.get(id=self.kwargs['pk'])
+        return context
+
 class ArticleDetailView(DetailView): # детализированное представление модели
     template_name = "article.html"
     model = Article 
