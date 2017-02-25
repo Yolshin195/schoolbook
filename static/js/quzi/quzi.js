afcf63f11f6bca22e8855191c007632c31be4350
Yolshin195 <elshin195@gmail.com>
2017-02-26 02:10:58 +0500

    модуль тестирования адаптирован к ie пробуем на сервере

--- static/js/quzi/quzi.js
+++ static/js/quzi/quzi.js
@@ -62,6 +62,18 @@ request.onreadystatechange = function(){
 }
 request.send();
 
+function quzi_comments (procent){
+    $.ajax({
+        type: "GET",
+        url: "/quzi/comments/" + procent + "/",
+        dataType: "json",
+        success: function (data){
+            var comment = document.getElementById("quzi_comments")
+            comment.innerHTML = data[0].fields.comment_test;
+        },
+    });
+}
+
 //Блок считывает и запоминает положение checkbox
 function check(number){
     // проверяет былли активизхирован хотябы один checkbox
@@ -148,4 +160,91 @@ function n(number){
     if (checboxvar[current]){
         checboxv(current);
     }
+}
+
+//Блок проверки ответов
+function check_answers(){
+        check(current);
+    var ind = 0;
+    for (var i = 0; i < answerarray.length; i++){
+        if (checboxvar[i]){
+                ind = 0; 
+            for(var j =0; j < checboxvar[i].length; j++ ){
+                if(checboxvar[i][j] == requestJSON[i].answer[j].ansver_boolean){
+                    ind = ind + 1;
+                }
+            }
+            if ('' + ind + '' == '' + checboxvar[i].length + ''){
+                answerarray[i] = true;
+            }else{
+                answerarray[i] = false;
+            }
+        }
+        
+    } 
+}
+
+// блок отрисовки ответа 
+function chart(){
+    check_answers();
+    var chart_true = 0;
+    var chart_false = 0;
+    var procent = 100 / requestJSON.length;
+    var progress;
+    var progress_content = '<div id="quzi_comments"></div>'
+        + '<div class="progress" id="progress"> </div>'
+		+ '<div class="list-group" id="reply">'
+		+ '</div>';
+    document.getElementById("quzi_content").innerHTML = progress_content;
+    var element_progress = document.getElementById("progress");
+    element_progress.innerHTML = "";
+    for (var i = 0; i < answerarray.length; i++){
+        if (answerarray[i]){
+            chart_true++; 
+            progress = '<div class="progress-bar progress-bar-success" name="progress_bar" style="width:' + procent + '%">'
+                + '<span class="sr-only">' + procent + '% Complete (danger)</span>'
+                + '</div>';
+
+            element_progress.innerHTML += progress;
+        }else{
+            chart_false++; 
+            progress = '<div class="progress-bar progress-bar-danger" name="progress_bar" style="width:' + procent + '%">'
+                + '<span class="sr-only">' + procent + '% Complete (danger)</span>'
+                + '</div>';
+
+            element_progress.innerHTML += progress;
+        } 
+    }
+    var amount_true = procent * chart_true;
+    switch (true){
+        case amount_true >= 0 && amount_true <= 20: 
+            amount_true = 20;
+            break;
+
+        case amount_true > 20 && amount_true <= 50:
+            amount_true = 50;
+            break;
+
+        case amount_true > 50 && amount_true <= 70:
+            amount_true = 70;
+            break;
+
+        case amount_true > 70 && amount_true <= 90:
+            amount_true = 90;
+            break;
+
+        case amount_true > 90 && amount_true <= 100:
+            amount_true = 100;
+            break;
+
+        default:
+            amount_true = 0;
+    }
+	var reply = '<a href="'+ location.href 
+		+'" class="alert list-group-item" onclik="start()">'
+    	+ '<p class="list-group-item-text">Правильных ответов '
+		+ chart_true +' из ' + requestJSON.length + '</p>'
+  		+ '</a>';
+    document.getElementById("reply").innerHTML = reply;
+    quzi_comments(amount_true);
 }
