c99a9c40d09d7524d8a0b983211480ba6d17ffd3
Yolshin195 <elshin195@gmail.com>
2017-02-25 18:24:56 +0500

    решаю проблему с работай quzi.js в ie

--- static/js/quzi/quzi.js
+++ static/js/quzi/quzi.js
@@ -1,37 +1,9 @@
-var quzi_data;
-var current = 0; //текущее положение
-var most_recent = $(".answer_id");
+most_recent = $(".answer_id")
 
-$.getJSON(
-    "/quzi/testJS/" + most_recent.attr('id') + "/",
-    onAjaxSuccess
-);
-
-function onAjaxSuccess(data){
-    
-    alert(data);
-    quzi_data = data;
- //   answer(data[current])
+var request = new XMLHttpRequest();
+request.open('GET', '/quzi/testJS/' + most_recent.attr('id') + '/', false);
+request.send();
+if(request.status===200){
+    console.log(request);
+    document.writeln(request.responseText);
 }
-
-/*
-function answer(content){
-// функция отрисовки страницы 
-    if (content){
-        $("#question").html(content.question_content);
-        $("#answer").empty();
-        for (fields of content.answer){ 
-            var answer = '<tr>'
-                + '<td style="width: 30px;" ><label>'
-                + '<input type="checkbox" name="checkbox" value="' + fields.ansver_boolean +'">'
-                + '</label></td>'
-                + '<td>' + fields.ansver_content + '</td>'
-                + '</tr>';
-            $("#answer").append(answer);
-        }
-    }
-    else{
-         alert("тест пройден")
-    }
-}
-*/
