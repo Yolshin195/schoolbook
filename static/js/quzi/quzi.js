09b0949d27e3b72db11b61d6668dcbe97b02d4e0
Yolshin195 <elshin195@gmail.com>
2017-02-25 19:30:36 +0500

    решаю проблему с работай quzi.js в ie

--- static/js/quzi/quzi.js
+++ static/js/quzi/quzi.js
@@ -1,15 +1,45 @@
 most_recent = $(".answer_id")
-
+//текущее положение
+var current = 0; 
+//инициализация ммасиива ответов
+var answerarray = [];
+var checboxvar = [];
 var request;
+var requestJSON;
+
+
+function answer(content){
+// функция отрисовки страницы 
+    if (content){
+        var question = document.getElementById("question");
+        question.innerHTML = content.question_content;
+        var answers = document.getElementById("answer");
+        answers.innerHTML = "";
+        for (fields of content.answer){ 
+            var answer = '<tr>'
+                + '<td style="width: 30px;" ><label>'
+                + '<input type="checkbox" name="checkbox" value="'
+                + fields.ansver_boolean +'">'
+                + '</label></td>'
+                + '<td>' + fields.ansver_content + '</td>'
+                + '</tr>';
+            answers.innerHTML += answer      
+        }
+    }
+}
+
 if (window.XMLHttpRequest) {
     request = new XMLHttpRequest();
 } else {
     request = new ActiveXObject("Microsoft.XMLHTTP");
 }
-request.open('GET', '/quzi/testJS/' + most_recent.attr('id') + '/', false);
+request.open('GET', '/quzi/testJS/' + most_recent.attr('id') + '/');
+request.onreadystatechange = function(){
+    if((request.readyState===4) && (request.status===200)){
+        requestJSON = JSON.parse(request.responseText);
+        alert(requestJSON);
+        
+    }
+}
 request.send();
 
-if(request.status===200){
-    var modify = document.getElementById('question');
-    modify.innerHTML = request.responseText;
-}
