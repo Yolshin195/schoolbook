8369babeabac04322fccb7f89fab90c4822d06ec
Yolshin195 <elshin195@gmail.com>
2017-02-25 16:59:27 +0500

    Асинхронный запрос и вывод содержимого на страницу

--- static/js/quzi/quzi.js
+++ static/js/quzi/quzi.js
@@ -1,4 +1,5 @@
 var quzi_data;
+var current = 0; //текущее положение
 var most_recent = $(".answer_id");
 
 $.getJSON(
@@ -7,6 +8,29 @@ $.getJSON(
 );
 
 function onAjaxSuccess(data){
+    
     alert(data);
+    quzi_data = data;
+    answer(data[current])
 }
 
+
+function answer(content){
+// функция отрисовки страницы 
+    if (content){
+        document.getElementById("question").innerHTML = content.question_content
+        document.getElementById("answer").innerHTML = ""
+        for (fields of content.answer){ 
+            var answer = '<tr>'
+                + '<td style="width: 30px;" ><label>'
+                + '<input type="checkbox" name="checkbox" value="' + fields.ansver_boolean +'">'
+                + '</label></td>'
+                + '<td>' + fields.ansver_content + '</td>'
+                + '</tr>';
+            document.getElementById("answer").innerHTML += answer      
+        }
+    }
+    else{
+         alert("тест пройден")
+    }
+}
